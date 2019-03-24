79df942dc1e27777887b44b953734da06fa6193e
PJ <33669966+PhillipJacobsen@users.noreply.github.com>
2019-03-24 13:52:52 -0600

    Update setupRoutines.ino

--- setupRoutines.ino
+++ setupRoutines.ino
@@ -1,4 +1,6 @@
-
+/********************************************************************************
+  This routine waits for a connection to your WiFi network according to "ssid" and "password" defined previously
+********************************************************************************/
 void setupWiFi() {
   tft.setCursor(0, 20);
   tft.setTextColor(ILI9341_WHITE);
@@ -23,12 +25,17 @@ void setupWiFi() {
 }
 
 
+/********************************************************************************
+  This routine configures the ESP32 internal clock to syncronize with NTP server.
+
+  apparently this will enable the core to periodically sync the time with the NTP server. I don't really know how often this happens
+  I am not sure if daylight savings mode works correctly. Previously it seems like this was not working on ESP2866
+********************************************************************************/
 void setupTime() {
-  //this sets the core to periodically sync the time. I don't really know how often this happens
-  //I am not sure if daylight savings mode works correctly. Previously it seems like this was not working on ESP2866
+
   configTime(timezone * 3600, dst, "pool.ntp.org", "time.nist.gov");
   // printLocalTime();
-  delay(100);    //After connecting to WiFi network display the IP address for 2 seconds before displaying the time
+  //  delay(100);
 
   //wait for time to sync from servers
   while (time(nullptr) <= 100000) {
@@ -39,22 +46,27 @@ void setupTime() {
   Serial.print("time is: ");
   Serial.println(ctime(&now));
 
-
-  //  tft.println("Time is");
-
-
   tft.setTextColor(ILI9341_WHITE);
   //  tft.setTextSize(1);
-  tft.print(ctime(&now));
+  tft.print(ctime(&now));      //dislay the current time
 
   //  struct tm * timeinfo;
   //  time(&now);
   //  timeinfo = localtime(&now);
   //  Serial.println(timeinfo->tm_hour);
-
 }
 
-
+/****************************************/
+//  https://lastminuteengineers.com/esp32-ntp-server-date-time-tutorial/
+//  void printLocalTime()
+//  {
+//    struct tm timeinfo;
+//    if (!getLocalTime(&timeinfo)) {
+//      Serial.println("Failed to obtain time");
+//      return;
+//    }
+//    Serial.println(&timeinfo, "%A, %B %d %Y %H:%M:%S");
+//  }
 
 
 
@@ -74,80 +86,67 @@ void ConfigureNeoPixels(RgbColor color) {
 
 
 
-/****************************************/
-//  https://lastminuteengineers.com/esp32-ntp-server-date-time-tutorial/
-//  void printLocalTime()
-//  {
-//    struct tm timeinfo;
-//    if (!getLocalTime(&timeinfo)) {
-//      Serial.println("Failed to obtain time");
-//      return;
-//    }
-//    Serial.println(&timeinfo, "%A, %B %d %Y %H:%M:%S");
-//  }
-
-
-
+/********************************************************************************
+  Configure peripherals and system
+********************************************************************************/
 void setup()
 {
-  Serial.begin(115200);     // Initialize Serial Connection for debug / display
+  Serial.begin(115200);         // Initialize Serial Connection for debug / display
 
-  tft.begin();              //setup TFT display
+  //--------------------------------------------
+  //  setup 240x320 TFT display with custom font and clear screen
+  // tft.setFont();    //configure standard adafruit font
+  tft.begin();
   tft.fillScreen(ILI9341_BLACK);  //clear screen
   tft.setFont(&FreeSans9pt7b);
-  // tft.setFont();    //standard font
-
-delay(300);
-  //--------------------------------------------
-  //Turn on all pixels so screen has a white background
-//  tft.fillScreen(ILI9341_WHITE);
-  //--------------------------------------------
-//  Turn on all pixels in the lower portion of the screen
-//   for (uint16_t x = 50; x < 239; x++) {
-//     for (uint16_t y = 90; y < 291; y++) {
-//       tft.drawPixel(x,y,ILI9341_WHITE);
-//       
-//     }
-//     delay(1);
-//   }
-//
-//  esp_deep_sleep_start();
-//  delay(3000);
-//  
-//  setupQRcode();
-
-
-  
-
-  setupWiFi();  //configure WiFi connection
-
-  setupTime();  //sync local time to NTP server
-
-  checkArkNodeStatus(); //check to see if Ark Node is synced
 
   //--------------------------------------------
   //  Configure NeoPixels.
-  //NOTE! Make sure to call strip.Begin() after you call Serial.Begin because Din pin of NeoPixel is also connected to Serial RX pin(on ESP8266) and will configure the pin for usage by the DMA interface.
+  //  NOTE! If useing the ESP8266 Make sure to call strip.Begin() after you call Serial.Begin because
+  //    Din pin of NeoPixel is also connected to Serial RX pin(on ESP8266) and will configure the pin for usage by the DMA interface.
   strip.Begin();
-  strip.Show(); // Initialize all pixels to 'off'
-  strip.ClearTo(RgbColor(0, 0, 0));
+//  strip.Show(); // Initialize all pixels to 'off'
+  strip.ClearTo(RgbColor(0, 0, 0)); // Initialize all pixels to 'off'
 
-  CursorX = tft.getCursorX();
-  CursorY = tft.getCursorY();
+  //--------------------------------------------
+  //setup WiFi connection
+  setupWiFi();
+
+  //--------------------------------------------
+  //  sync local time to NTP server
+  setupTime();
+
+  //--------------------------------------------
+  //  check to see if Ark Node is synced
+  //  node is defined previously with "peer" and "port"
+  checkArkNodeStatus();
+
+
+  //--------------------------------------------
+  //  We are now going to find the last transaction received in wallet defined previously in "ArkAddress"
+  //  We will start from the oldest received transaction and keep reading transactions one at a time until we have read them all.
+  //  Because we read 1 trasactions at a time the page number returned by the API tells us how many transactions there are.
+  //  Every transaction received will toggle the color of "searching wallet: " text between red and white.
+  //  Once we have read all the transactions we will display the total number of transactions in wallet.
+  //  
+  CursorX = tft.getCursorX();     //get current cursor position
+  CursorY = tft.getCursorY();     //get current cursor position
   tft.println("searching wallet: ");
   tft.println(ArkAddress);
 
-  lastRXpage = getMostRecentReceivedTransaction();  //lastRXpage is equal to the page number of the last received transaction.
+  lastRXpage = getMostRecentReceivedTransaction();  //lastRXpage is equal to the page number of the last received transaction in the wallet.
 
-  tft.setTextColor(ILI9341_WHITE);
+  tft.setTextColor(ILI9341_WHITE);   
   tft.setCursor(CursorX, CursorY);
   tft.println("searching wallet: ");
   tft.println(ArkAddress);
 
   tft.print("# of transactions in wallet: ");
-  tft.println(lastRXpage);
+  tft.println(lastRXpage);          //this is the page number of the last received transaction. This is also the total number of transactions in the wallet
 
 
+  //--------------------------------------------
+  //  System is now configured! Set Neo Pixels to Green
   ConfigureNeoPixels(redgreen);
 
 }
